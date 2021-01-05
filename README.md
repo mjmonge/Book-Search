9ca3490bb1dbbe68f58add6e56b2d8cfe6a5b6a3
mjmonge <matthew.monge@marquette.edu>
2021-01-05 17:24:20 -0600

    Uploaded search app

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# Book-Search
+# Google Book Search api
+
+- A create-react-app Thinkful drill
+
+Assignment requirements:
+
+- Provide a search form to allow users to search for books by some search term that they enter.
+- Allow filters such as type of book or whether the book is a free ebook or not.
+- Display the search result in list and optionally allow the user to click a result to see further details of the book.
+- Use the Google Books API to perform the search
---
+++ src/App.js
@@ -0,0 +1,115 @@
+import React, { Component } from "react";
+import BookSearch from "./bookSearch/bookSearch";
+
+import "./App.css";
+import BookSearchList from "./bookSearchList/bookSearchList";
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isPrintType: "all",
+      isBookType: "no-filter",
+      searchEntry: "",
+      searchResults: [],
+      error: null
+    };
+  }
+
+
+  setPrintSelected(sel) {
+    console.log("Print Selected:", sel);
+    this.setState({
+      isPrintType: sel
+    });
+  }
+
+  setBookSelected(sel) {
+    console.log(" BOOK selected:", sel);
+    this.setState({
+      isBookType: sel
+    });
+  }
+
+  searchInput(inp) {
+    console.log("Search has been activated. Search entry is: ", inp);
+    this.setState({
+      searchEntry: inp
+    });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    console.log("submit handled!");
+
+    //Creating a query
+    const baseUrl =
+      "https://www.googleapis.com/books/v1/volumes?q=search+terms";
+    let printType = `$printType=${this.state.isPrintType}`;
+    let filter =
+      this.state.isBookType !== "no-filter"
+        ? `$filter=${this.state.isBookType}`
+        : "";
+    let searchEntry = `${this.state.searchEntry}`;
+
+    const queryString = `${baseUrl}?q=${searchEntry}&${filter}&${printType}`;
+
+    console.log(queryString);
+
+    //perform search
+    fetch(queryString)
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error();
+      })
+      .then(responseJSon => {
+        console.log(responseJSon);
+        this.setState({
+          searchResults: responseJSon.items
+        });
+      })
+      .catch(e => {
+        console.log(e);
+        this.setState({ error: e.message });
+      });
+  }
+
+  render() {
+    const selectOptions = {
+      printSelections: ["all", "books", "magazines"],
+      bookSelections: [
+        "no-filter",
+        "partial",
+        "full",
+        "free-ebooks",
+        "paid-ebooks",
+        "ebooks"
+      ]
+    };
+
+    const error = this.state.error ? (
+      <div className="error">{this.state.error}</div>
+    ) : (
+      ""
+    );
+    return (
+      <div className="App">
+        <BookSearch
+          selectOptions={selectOptions}
+          printChangeHandler={sel => this.setPrintSelected(sel)}
+          bookChangeHandler={sel => this.setBookSelected(sel)}
+          handleSubmit={e => this.handleSubmit(e)}
+          handleSearchInput={inp => this.searchInput(inp)}
+        />
+        <BookSearchList filterBookInfo={this.state.searchResults} />
+        {error}
+      </div>
+    );
+  }
+}
+
+App.defaultProps = {
+  searchResults: []
+};
