2053b79b3a6f4db7b8164d2d645957bd4eed70fe
mjmonge <matthew.monge@marquette.edu>
2021-01-05 17:23:09 -0600

    Updates

---
+++ precache-manifest.82e2647d16041df85a8b687539ce55d0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "132d6bfd33c4113fc83ea11d75c9c61f",
+    "url": "/Book-Search/index.html"
+  },
+  {
+    "revision": "68a7b44befbed19c4260",
+    "url": "/Book-Search/static/css/main.ba49ec77.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/Book-Search/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "68a7b44befbed19c4260",
+    "url": "/Book-Search/static/js/main.7160f10e.chunk.js"
+  },
+  {
+    "revision": "8f2322dda0285431db4e",
+    "url": "/Book-Search/static/js/runtime~main.fd3f113d.js"
+  }
+]);
